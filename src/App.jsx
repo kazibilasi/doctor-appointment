c00dd16c77cb5827d0a4a23fc385e3fc9d6da230
Kazi Bilasi <kazibilasi@gmail.com>
2024-09-30 11:01:22 +0600

    working on navbar

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
 import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import Doctors from "./pages/Doctors";
+import Login from "./pages/Login";
+import Contact from "./pages/Contact";
+import About from "./pages/About";
+import MyProfile from "./pages/MyProfile";
+import MyAppointments from "./pages/MyAppointments";
+import Appointment from "./pages/Appointment";
+import Navbar from "./components/Navbar";
 
 const App = () => {
-  return <div className=" text-green-400" >hello pain</div>;
+  return (
+    <div className=" mx-4 sm:mx-[10%]">
+      <Navbar></Navbar>
+      <Routes>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/doctors" element={<Doctors></Doctors>}></Route>
+        <Route path="/doctors/:speciality" element={<Doctors></Doctors>}></Route>
+        <Route path="/login" element={<Login></Login>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="/contact" element={<Contact></Contact>}></Route>
+        <Route path="/my-profile" element={<MyProfile></MyProfile>}></Route>
+        <Route path="/my-appointments" element={<MyAppointments></MyAppointments>}></Route>
+        <Route path="/appointment/:docId" element={<Appointment></Appointment>}></Route>
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
