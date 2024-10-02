7d33ca72dc08b8f168724f3d254ffae264c6797f
Kazi Bilasi <kazibilasi@gmail.com>
2024-10-02 13:40:23 +0600

    working on footer

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,12 +2,14 @@ import React from "react";
 import Header from "../components/Header";
 import SpecialityMenu from "../components/SpecialityMenu";
 import TopDoctors from "../components/TopDoctors";
+import Banner from "../components/Banner";
 
 const Home = () => {
   return <div>
    <Header></Header>
    <SpecialityMenu></SpecialityMenu>
    <TopDoctors></TopDoctors>
+   <Banner></Banner>
   </div>;
 };
 
